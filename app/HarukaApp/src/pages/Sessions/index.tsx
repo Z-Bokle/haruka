30ce0e9bef2552b7baac2cd25ff86a5ecc058fa3
Bokle Zhu <Bokle@foxmail.com>
2024-03-09 20:58:47 +0800

    feat: FAB

--- app/HarukaApp/src/pages/Sessions/index.tsx
+++ app/HarukaApp/src/pages/Sessions/index.tsx
@@ -10,11 +10,13 @@ import {
 import SessionCard, { Session } from '../../components/SessionCard';
 import { useNetwork } from '../../utils/Network';
 import { session } from '../../api';
+import { AnimatedFAB } from 'react-native-paper';
 
 function Sessions() {
   const [sessions, setSessions] = useState<Session[]>([]);
 
   const [refreshing, setRefreshing] = useState(false);
+  const [extendFAB, setExtendFAB] = useState(false);
 
   const { jsonGet } = useNetwork();
 
@@ -42,6 +44,20 @@ function Sessions() {
           refreshControl={
             <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
           }
+          onScroll={({ nativeEvent }) => {
+            const currentScrollPosition =
+              Math.floor(nativeEvent.contentOffset.y) ?? 0;
+            setExtendFAB(currentScrollPosition > 20);
+          }}
+        />
+        <AnimatedFAB
+          icon="plus"
+          label="新建会话"
+          onPress={() => console.log('pressed')}
+          extended={extendFAB}
+          visible={!refreshing}
+          animateFrom="right"
+          style={style.fab}
         />
       </SafeAreaView>
     </View>
@@ -59,6 +75,12 @@ const style = StyleSheet.create({
   item: {
     width: '100%',
     height: 200,
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
   },
 });
 
