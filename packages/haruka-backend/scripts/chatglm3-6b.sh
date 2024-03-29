dee87593bb9947a7591c1869a92fe0fd5469a6cc
Bokle Zhu <Bokle@foxmail.com>
2024-03-29 18:10:48 +0800

    fix: delete desc

--- packages/haruka-backend/scripts/chatglm3-6b.sh
+++ packages/haruka-backend/scripts/chatglm3-6b.sh
@@ -24,26 +24,3 @@ curl -s -X POST --location $3 \
         "result_format": "message"
     }
 }' | jq '.output.choices[0].message.content'
-
-
-# curl --location 'https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation' \
-# --header 'Authorization: Bearer krdfBS4VBe' \
-# --header 'Content-Type: application/json' \
-# --data '{
-#     "model": "chatglm3-6b",
-#     "input":{
-#         "messages":[      
-#             {
-#                 "role": "system",
-#                 "content": "You are a helpful assistant."
-#             },
-#             {
-#                 "role": "user",
-#                 "content": "你好，请介绍一下故宫"
-#             }
-#         ]
-#     },
-#     "parameters": {
-#         "result_format": "message"
-#     }
-# }'
