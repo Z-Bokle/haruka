05049526a4ecc1f89218a9cbbb191335ddd01bc9
Bokle Zhu <Bokle@foxmail.com>
2024-04-02 16:23:09 +0800

    feat: test

--- packages/haruka-backend/scripts/audio.sh
+++ packages/haruka-backend/scripts/audio.sh
@@ -1,6 +1,8 @@
 # write text to file
-echo $1 > "/tmp/haruka/$2.txt"
-# echo $1
-# echo $2
+# echo $1 > "/tmp/haruka/$2.txt"
+echo 文本
+echo $1
+echo 文件路径
+echo "/tmp/haruka/$2.txt"
 # run tts program
-~/workspace/SummerTTS/build/tts_test "/tmp/haruka/$2.txt" ~/workspace/SummerTTS/models/single_speaker_fast.bin "../statics/audio/$2.wav"
+# ~/workspace/SummerTTS/build/tts_test "/tmp/haruka/$2.txt" ~/workspace/SummerTTS/models/single_speaker_fast.bin "../statics/audio/$2.wav"
