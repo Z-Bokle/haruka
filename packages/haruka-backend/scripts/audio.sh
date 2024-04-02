c21120cd8c6a521751a14a2c7b9f42fd6123589c
Bokle Zhu <Bokle@foxmail.com>
2024-04-02 17:22:43 +0800

    Revert "fix: remove '"
    This reverts commit 68976cedbfd8ee4fc0e0722f4156da6be08b57bc.

--- packages/haruka-backend/scripts/audio.sh
+++ packages/haruka-backend/scripts/audio.sh
@@ -1,6 +1,6 @@
 # write text to file
-touch "/tmp/haruka/$2.txt"
-printf "%s\n" $1 > "/tmp/haruka/$2.txt"
+touch "'/tmp/haruka/$2.txt'"
+printf "%s\n" $1 > "'/tmp/haruka/$2.txt'"
 
 # run tts program
-~/workspace/SummerTTS/build/tts_test "/tmp/haruka/'$2'.txt" ~/workspace/SummerTTS/models/single_speaker_fast.bin "../statics/audio/'$2'.wav"
+~/workspace/SummerTTS/build/tts_test "'/tmp/haruka/$2.txt'" ~/workspace/SummerTTS/models/single_speaker_fast.bin "'../statics/audio/$2.wav'"
