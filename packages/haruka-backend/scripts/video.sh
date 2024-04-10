b49de33d7e13d733533fcfb9febb05cb118ab96e
Bokle Zhu <Bokle@foxmail.com>
2024-04-10 17:15:38 +0800

    fix: shell error

--- packages/haruka-backend/scripts/video.sh
+++ packages/haruka-backend/scripts/video.sh
@@ -1,11 +1,11 @@
 eval "$(conda shell.bash hook)"
-conda active video_retalking
+conda activate video_retalking
 
 # baseVideoFilePath=$1
 # audioFilePath=$2
 # targetFilePath=$3
 
-python3 ~/workspace/video_retalking/inference.py \
+python3 ~/workspace/video-retalking/inference.py \
   --face "$1" \
   --audio "$2" \
   --outfile "$3"
