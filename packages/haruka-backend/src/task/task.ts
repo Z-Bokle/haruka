2ec128e54cf196f7ad68195991245977a496f294
Bokle Zhu <Bokle@foxmail.com>
2024-04-13 16:31:15 +0800

    test

--- packages/haruka-backend/src/task/task.ts
+++ packages/haruka-backend/src/task/task.ts
@@ -214,7 +214,7 @@ export class VideoTask extends Task<VideoTaskResult> {
 
       try {
         const cp = execFile('bash', [scriptFilePath, ...args], {
-          cwd: '~/workspace/video-retalking',
+          // cwd: '~/workspace/video-retalking',
           shell: true,
         });
 
